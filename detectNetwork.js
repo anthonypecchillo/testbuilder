abf764d4a1f24aaa7d8de2cc9da2eb383abfb648
anthonypecchillo <anthonypecchillo@gmail.com>
2016-11-25 20:43:20 -0800

    Added functionality for China UnionPay and Switch credit cards

--- detectNetwork.js
+++ detectNetwork.js
@@ -16,26 +16,34 @@ var detectNetwork = function(cardNumber) {
   var prefix01 = cardNumber[0] + cardNumber[1];
   var prefix2 = cardNumber[2];
   var prefix3 = cardNumber[3];
+  prefix45 = cardNumber[4] + cardNumber[5];
   var dcPrefixes = ["38", "39"];
   var aePrefixes = ["34", "37"];
   var mcPrefixes = ["51", "52", "53", "54", "55"];
   var discPrefixes = ["65", "644", "645", "646", "647", "648", "649", "6011"];
   var maesPrefixes = ["5018", "5020", "5038", "6304"];
+  var cupPrefixes = [];
+  var switchPrefixes = ["4903", "4905", "4911", "4936", "6333", "6759", "564182", "633110"];
   var visaLengths = [13, 16, 19];
   var discLengths = [16, 19];
   var maesLengths = [12, 13, 14, 15, 16, 17, 18, 19];
+  var cupLengths = [16, 17, 18, 19];
+  var switchLengths = [16, 18, 19];
 
-  // var testPrefix = function(element) {
-  // 	return element === prefix;
-  // };
+  for (var i = 624; i <= 622925; i++) {
+    cupPrefixes.push(i);
+    if (i === 626) { i = 6281; } else if (i === 6288) { i = 622125; }
+  }
 
   var testPrefix = function(element) {
   	if (element.length === 2) {
   		return element === prefix01;
   	} else if (element.length === 3) {
   		return element === prefix01 + prefix2;
-  	} else {
+  	} else if (element.length === 4) {
   		return element === prefix01 + prefix2 + prefix3;
+  	} else if (element.length === 6) {
+  		return element === prefix01 + prefix2 + prefix3 + prefix45;
   	}
   };  
 
@@ -55,6 +63,10 @@ var detectNetwork = function(cardNumber) {
     return "Discover";
   } else if (maesLengths.some(testLength) && maesPrefixes.some(testPrefix)) {
     return "Maestro";
+  } else if (cupLengths.some(testLength) && cupPrefixes.some(testPrefix)) {
+  	return "China UnionPay";
+  } else if (switchLengths.some(testLength) && switchPrefixes.some(testPrefix)) {
+    return "Switch";
   }
 };
 
